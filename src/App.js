88980ca5e13598b97d239b5adaca22f76c7fc8a6
Vardan GHULYAN <vghulyan@yahoo.com>
2021-01-20 21:29:59 +0000

    Headline component with tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './component/header';
+import Headline from "./component/headline";
 import scss from './app.scss';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Header />
+      <section className="main">
+          <Headline header='Exchange' desc='Click the button to render exchange!' />
+      </section>
     </div>
   );
 }
