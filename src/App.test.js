2eb8bb2fd283946168137dbcb2f54d8b0153b78a
Vardan GHULYAN <vghulyan@yahoo.com>
2021-01-21 17:39:40 +0000

    using husky pre-comit hooks

--- src/App.test.js
+++ src/App.test.js
@@ -28,9 +28,4 @@ describe('App Component', () => {
         expect(component.length).toBe(1);
     });
 
-    it('Should render appComponent without errors', () => {
-        const component = findByTestAttr(wrapper, 'appComponent');
-        expect(component.length).toBe(1);
-    });
-
 });
